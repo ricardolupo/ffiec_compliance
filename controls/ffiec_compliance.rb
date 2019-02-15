04c384627c9665f696d0f95d983504ab9a14fb31
chef <chef@chef.io>
2019-02-15 22:44:47 +0000

    initial commit

---
+++ controls/ffiec_compliance.rb
@@ -0,0 +1,7 @@
+require_controls 'ffiec_compliance' do
+    control 'xccdf_org.cisecurity.benchmarks_rule_1.4.2_Ensure_bootloader_password_is_set'
+    control 'xccdf_org.cisecurity.benchmarks_rule_1.2.1_Ensure_package_manager_repositories_are_configured' do
+        impact 0.8
+    end
+    control 'xccdf_org.cisecurity.benchmarks_rule_5.6_Ensure_access_to_the_su_command_is_restricted'
+end
