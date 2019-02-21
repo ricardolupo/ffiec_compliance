735bd0e1516f299d45e7854f3fda9d8bab6c41a8
chef <chef@chef.io>
2019-02-21 17:55:11 +0000

    refactored controls

--- controls/ffiec.rb
+++ controls/ffiec.rb
@@ -1,7 +1,21 @@
-require_controls 'cis-ubuntu16.04lts-level1-server' do
-    control 'xccdf_org.cisecurity.benchmarks_rule_1.4.2_Ensure_bootloader_password_is_set'
-    control 'xccdf_org.cisecurity.benchmarks_rule_1.2.1_Ensure_package_manager_repositories_are_configured' do
-        impact 0.8
-    end
-    control 'xccdf_org.cisecurity.benchmarks_rule_5.6_Ensure_access_to_the_su_command_is_restricted'
+include_controls 'cis-ubuntu16.04lts-level1-server' do
+    skip_control 'xccdf_org.cisecurity.benchmarks_rule_1.1.17_Ensure_nodev_option_set_on_removable_media_partitions'
+    skip_control 'xccdf_org.cisecurity.benchmarks_rule_1.1.18_Ensure_nosuid_option_set_on_removable_media_partitions'
+    skip_control 'xccdf_org.cisecurity.benchmarks_rule_1.1.19_Ensure_noexec_option_set_on_removable_media_partitions'
+    skip_control 'xccdf_org.cisecurity.benchmarks_rule_1.2.1_Ensure_package_manager_repositories_are_configured'
+    skip_control 'xccdf_org.cisecurity.benchmarks_rule_1.2.2_Ensure_GPG_keys_are_configured'
+    skip_control 'xccdf_org.cisecurity.benchmarks_rule_3.3.3_Ensure_IPv6_is_disabled'
 end
+
+control "xccdf_org.cisecurity.benchmarks_rule_3.3.3_Ensure_IPv6_is_disabled" do
+    title "Ensure IPv6 is disabled"
+    desc  "
+      Although IPv6 has many advantages over IPv4, few organizations have implemented IPv6.
+      
+      Rationale: If IPv6 is not to be used, it is recommended that it be disabled to reduce the attack surface of the system.
+    "
+    impact 0.5
+    describe file("/boot/grub/grub.cfg") do
+      its("content") { should match(/^\s*linux\S*(\s+\S+)+\s+ipv6\.disable=1/) }
+    end
+  end
